68d0f88ac948048992734c8072b0930a0130221d
Sindrit17 <sindrit17@ru.is>
2021-02-21 22:43:47 +0000

    Update PCB.cpp
    PCB

--- lib/PCB.cpp
+++ lib/PCB.cpp
@@ -6,7 +6,7 @@ using namespace std;
 class PCB
 {
 private:
-    int _state;
+    bool _state;
     PCB* _parent;
     queue<PCB*> _children;
     queue<int*>  _resources; // index of the rcb
@@ -17,14 +17,18 @@ public:
     PCB(int priority);
     ~PCB();
     int test();
+    void changeState();
     void setParent(PCB *process);
     void addChild(PCB *process);
+    void addResources(int *resource);
+    void removeResource();
     friend ostream& operator<<(ostream& out, const PCB *pcb);
+    
 };
 
 PCB::PCB(int priority) {
     // TODO: child and resources = null
-    _state = READY;
+    _state = true;
     _priority = priority;
     
 }
@@ -32,13 +36,22 @@ PCB::PCB(int priority) {
 PCB::~PCB() {
 
 }
-
+void PCB::changeState(){
+    _state = !_state;
+}
 void PCB::setParent(PCB *process) {
     _parent = process;
 }
 
 void PCB::addChild(PCB *process) {
     _children.push(process);
+}
+void PCB::addResources(int *resource){
+    _resources.push(resource);
+}
+
+void PCB::removeResource(){
+    _resources.pop();
 }
 
 ostream& operator<<(ostream& out, const PCB *pcb){
